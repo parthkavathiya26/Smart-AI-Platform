7106a1dd5349bffafac66d1b3f05a2984f508ef7
Prince026 <pckavathiya1@gmail.com>
2026-01-12 11:23:37 +0530

    Changes made by Prince

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
 from backend.api import chatbot
 
+app = FastAPI()
+
 app.include_router(chatbot.router, prefix="/chatbot")
+
+@app.get("/")
+def home():
+    return {"status": "Smart AI Platform running"}
+
